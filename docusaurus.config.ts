55c0181c11147c50f30a7d5568b04ac4cd331870
Eunseo Choi <eunseo.choi@gmail.com>
2025-02-12 12:25:37 -0600

    trying deploy on github page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/des3d/',
+  projectName: 'geoflac.github.io',
+  organizationName: 'geoflac',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
