6196c500a002a5b072774b9e6d6c1b22d83bdc02
Eunseo Choi <echoi@users.noreply.github.com>
2025-05-08 19:22:49 -0500

    Update docusaurus.config.ts
    katex stylesheet updated

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -153,7 +153,13 @@ const config: Config = {
       {
         href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
         type: 'text/css',
+        integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+        crossorigin: 'anonymous',
       },
+      //{
+      //  href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      //  type: 'text/css',
+      //},
     ],
   } satisfies Preset.ThemeConfig,
 };
